1ba5b625ba38a4182cea627c3a535132e30a45ab
Jonas Roslindo <jonas.roslindo@gmail.com>
2026-09-06 09:26:40 -0300

    Estilização e script atualizado

--- script.js
+++ script.js
@@ -1,74 +1,125 @@
-const modal = document.getElementById("meuModal");
-const abrir = document.getElementById("abrirModal");
-const fechar = document.getElementById("fecharModal");
+
+const modal = document.getElementById("meuModal")
+const abrir = document.getElementById("abrirModal")
+const fechar = document.getElementById("fecharModal")
+const salvar = document.querySelector('#salvarModal')
+const filtroSolicitante = document.querySelector('#filtroSolicitante')
+const filtroBloco = document.querySelector('#filtroBloco')
+const filtroSala = document.querySelector('#filtroSala')
+const filtroData = document.querySelector('#filtroData')
+const filtroTurno = document.querySelector('#filtroTurno')
 
 abrir.addEventListener("click", () => {
-  modal.showModal();
-});
+  modal.showModal()
+})
 
 fechar.addEventListener("click", () => {
-  modal.close();
-});
+  modal.close()
+  limparModal()
+})
 
-const turma = []
+salvar.addEventListener('click', () => {
+  if (cadastrar()) {
+    fazerTabela()
+    limparModal()
+    modal.close()
+  }
+})
 
-function definirSituacao(situacao) {
-    const nome = document.getElementById('iptNome').value
-    const n1 = Number(document.getElementById('iptNota1').value)
-    const n2 = Number(document.getElementById('iptNota2').value)
-    console.log(nome, n1, n2)
+const reserva = []
 
-    const media = (n1 + n2) / 2
+function limparModal() {
+  document.querySelector('#solicitante').value = ''
+  document.querySelector('#bloco').selectedIndex = 0
+  document.querySelector('#sala').selectedIndex = 0
+  document.querySelector('#data').value = ''
+  document.querySelector('#turno').selectedIndex = 0
+}
 
-    console.log(media)
+function cadastrar() {
+    const solicitante = document.querySelector('#solicitante').value
+    const bloco = document.querySelector('#bloco').value
+    const sala = document.querySelector('#sala').value
+    const data = document.querySelector('#data').value
+    const turno = document.querySelector('#turno').value
 
-    if (media >= 7.0) {situacao = 'Aprovado'}
-    else if (media >= 5.0 && media < 7.0) {situacao = 'Recuperação'}
-    else {situacao = 'Reprovado'}
-
-    console.log(situacao)
-
-    const aluno = {
-        Nome: nome,
-        Nota1: n1,
-        Nota2: n2,
-        Média: media,
-        Situação: situacao
+    const cadastro = {
+      Solicitante: solicitante,
+      Bloco: bloco,
+      Sala: sala,
+      Data: data,
+      Turno: turno
     }
 
-    turma.push(aluno)
+    const cadastroDuplicado = reserva.some(item =>
+      item.Solicitante === cadastro.Solicitante &&
+      item.Bloco === cadastro.Bloco &&
+      item.Sala === cadastro.Sala &&
+      item.Data === cadastro.Data &&
+      item.Turno === cadastro.Turno
+    )
+
+    if (cadastroDuplicado) {
+      alert('Esta reserva já foi cadastrada.')
+      return false
+    }
+
+    reserva.push(cadastro)
+    return true
 }
 
-function renderizarTabela(alunosFiltrados) {
-    const tbody = document.querySelector('#tblAlunos')
-    const filter = document.querySelector('#sltFiltro').value
-    
-    if (filter === 'Todos') { alunosFiltrados = turma } 
-    else if (filter === 'Aprovados') { alunosFiltrados = turma.filter(aluno => aluno.Situação === 'Aprovado') } 
-    else if (filter === 'Reprovados') { alunosFiltrados = turma.filter(aluno => aluno.Situação === 'Reprovado') } 
-    else if (filter === 'Recuperação') { alunosFiltrados = turma.filter(aluno => aluno.Situação === 'Recuperação') }
 
-    tbody.innerHTML = alunosFiltrados.map(({Nome, Nota1, Nota2, Média, Situação}) =>
-        `<tr>
-            <td>${Nome}</td>
-            <td>${Nota1}</td>
-            <td>${Nota2}</td>
-            <td>${Média}</td>
-            <td>${Situação}</td>
+function fazerTabela() {
+  const tbody = document.querySelector('#tabela')
+
+  const reservasFiltradas = filtrarReservas()
+
+  tbody.innerHTML = reservasFiltradas.map(({ cadastro, indice }) => {
+    const { Solicitante, Bloco, Sala, Data, Turno } = cadastro
+
+    return `
+      <tr>
+            <td>${Solicitante}</td>
+            <td>${Bloco}</td>
+            <td>${Sala}</td>
+            <td>${Data}</td>
+            <td>${Turno}</td>
+            <td><button class="limpar" data-indice="${indice}">Limpar</button></td>
         </tr>`
-    ).join('')
+  }).join('')
+
 }
 
-function cadastrarAluno() {
-    const button = document.querySelector('#btnCadastrar')
-    const filtro = document.querySelector('#sltFiltro')
+function filtrarReservas() {
+  const textoSolicitante = filtroSolicitante.value.toLowerCase()
 
-    button.addEventListener('click', () => {
-        definirSituacao()
-        renderizarTabela()
-        })
+  return reserva
+    .map((cadastro, indice) => ({ cadastro, indice }))
+    .filter(({ cadastro }) => {
+      const mesmoSolicitante = cadastro.Solicitante
+        .toLowerCase()
+        .includes(textoSolicitante)
+      const mesmoBloco = filtroBloco.value === '' || cadastro.Bloco === filtroBloco.value
+      const mesmaSala = filtroSala.value === '' || cadastro.Sala === filtroSala.value
+      const mesmaData = filtroData.value === '' || cadastro.Data === filtroData.value
+      const mesmoTurno = filtroTurno.value === '' || cadastro.Turno === filtroTurno.value
 
-    filtro.addEventListener('change', renderizarTabela)
+      return mesmoSolicitante && mesmoBloco && mesmaSala && mesmaData && mesmoTurno
+    })
+
 }
 
-cadastrarAluno()
+filtroSolicitante.addEventListener('input', fazerTabela)
+filtroBloco.addEventListener('change', fazerTabela)
+filtroSala.addEventListener('change', fazerTabela)
+filtroData.addEventListener('change', fazerTabela)
+filtroTurno.addEventListener('change', fazerTabela)
+
+document.querySelector('#tabela').addEventListener('click', event => {
+  if (!event.target.classList.contains('limpar')) {
+    return
+  }
+
+  reserva.splice(Number(event.target.dataset.indice), 1)
+  fazerTabela()
+})
